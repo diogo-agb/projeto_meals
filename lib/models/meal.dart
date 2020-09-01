674884c21d6dacad877916347fac814316a340b2
diogoagb <diogo.agb@uni9.edu.br>
2020-08-31 22:05:37 -0300

    updated meals and details the meal

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -42,4 +42,30 @@ class Meal {
     @required this.complexity,
     @required this.cost,
   });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return "Simples";
+      case Complexity.Medium:
+        return "Média";
+      case Complexity.Difficult:
+        return "Difícil";
+      default:
+        return "Desconhecida";
+    }
+  }
+
+  String get costText {
+    switch (cost) {
+      case Cost.Cheap:
+        return "Baixo";
+      case Cost.Fair:
+        return "Medio";
+      case Cost.Expensive:
+        return "Alto";
+      default:
+        return "Desconhecido";
+    }
+  }
 }
